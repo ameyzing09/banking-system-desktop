7ae9cdc715d689a51ab4cc5a5cb616abf62c7c3d
= <=>
2022-04-10 16:53:15 +0530

    Added electron

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 1920,
+      height: 1080
+    })
+  
+    win.loadURL('http://localhost:3000/accountOpening')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
